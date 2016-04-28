27bca8ed27e3865655a494b77948b54bcb7b26e0
hulksyed07 <hulk.syed07@gmail.com>
2016-04-28 15:04:36 +0530

    replaced include with extends

--- lib/url_encrypt_loader.rb
+++ lib/url_encrypt_loader.rb
@@ -3,7 +3,7 @@ class Engine < Rails::Engine
   initializer "url_encrypt" do
     require 'url_encrypt'
 
-    ActiveRecord::Base.send(:include, UrlEncrypt)
+    ActiveRecord::Base.send(:extend, UrlEncrypt)
   end
 
 end
